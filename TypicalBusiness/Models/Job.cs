587b139b4f2d6d8717bbc9cf923d336b411241f1
jjlane4 <jml7128@uncw.edu>
2016-02-24 10:52:22 -0500

    Created model to use later for the table on the career view

---
+++ TypicalBusiness/Models/Job.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TypicalBusiness.Models
+{
+    public class Job
+    {
+        
+        public int JobID { get; set; }
+        
+        public String Title { get; set; }
+    }
+}
