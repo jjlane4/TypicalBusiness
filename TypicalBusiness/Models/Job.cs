25c4483d433a3fb8d988d82f529f74a9e3e8e0ea
jjlane4 <jml7128@uncw.edu>
2016-05-02 12:28:24 -0400

    Added a dateTime property to the jobs model to show the date the job was posted.It was added to the details page. It is a readOnly property and does not appear on the edit page.

--- TypicalBusiness/Models/Job.cs
+++ TypicalBusiness/Models/Job.cs
@@ -8,12 +8,25 @@ namespace TypicalBusiness.Models
 {
     public class Job
     {
+
+        public Job()
+        {
+            this.DateCreated = DateTime.Now;
+        }
         
+        [Display(Name = "JobID")]
         public int JobID { get; set; }
-        
+
+        [Display(Name = "JobTitle")]
         public String Title { get; set; }
 
+        [Display(Name = "Date Posted")]
+        [DisplayFormat(DataFormatString="{0:dd/MM/yyyy}")]
+        public DateTime DateCreated { get; set; }
+
+        [Display(Name = "Description")]
         [DataType(DataType.MultilineText)]
         public String Description { get; set; }
+
     }
 }
